9cfb67bf118614faa15cad83313bb937b033c460
rev1853 <rghofur053@gmail.com>
2023-11-19 19:12:01 +0700

    done

--- src/APIRequester.ts
+++ src/APIRequester.ts
@@ -1,11 +1,11 @@
-import Axios, { AxiosInstance } from "axios"
+import Axios, { AxiosError, AxiosInstance } from "axios"
 import { ApiError } from "./Baaanggg"
 import { TokenResponse } from "./dto/response"
 
 export class APIRequester {
     axios: AxiosInstance
 
-    constructor(baseURL: string, settings: APIRequesterSettings) {
+    constructor(baseURL: string, private settings: APIRequesterSettings) {
         this.axios = Axios.create({
             baseURL: baseURL,
         })
@@ -16,15 +16,57 @@ export class APIRequester {
 
             return config
         }, error => Promise.reject(error))
+
+        this.axios.interceptors.response.use(
+            res => res,
+            async (error: AxiosError) => {
+                const request = error.config as any
+                const paths = error.config!.url!.split('/')
+                const lastPath = paths[paths.length - 1]
+
+
+                if (error.response?.status === 401 && !request._retry && lastPath != "refresh") {
+                    const token = settings.getToken()
+                    if (!token) return Promise.reject(error);
+                    request._retry = true;
+
+                    let newToken;
+                    const count = 0;
+                    while (count < 3) {
+                        try {
+                            newToken = await this.refreshToken(token)
+                        } catch { }
+                    }
+
+                    if (newToken) {
+                        settings.onAuthenticated(newToken)
+                        request.headers.Authorization = `Bearer ${newToken.accessToken}`;
+                        return this.axios(request);
+                    }
+                }
+
+                return Promise.reject(error);
+            }
+        )
     }
+
+    async refreshToken(token: TokenResponse): Promise<TokenResponse | undefined> {
+        try {
+            const response = await this.post<TokenResponse>('/auth/refresh', {
+                refreshToken: token.refreshToken,
+            });
+            return response;
+        } catch (error) {
+            return undefined;
+        }
+    };
 
     async post<T>(endpoint: string, data?: any): Promise<T> {
         try {
             const res = await this.axios.post(endpoint, data)
             return res.data
         } catch (e: any) {
-            if ('response' in e && 'data' in e.response) throw new ApiError(e.response.data)
-            throw e
+            throw this.handleError(e)
         }
     }
 
@@ -33,8 +75,7 @@ export class APIRequester {
             const res = await this.axios.put(endpoint, data)
             return res.data
         } catch (e: any) {
-            if ('response' in e && 'data' in e.response) throw new ApiError(e.response.data)
-            throw e
+            throw this.handleError(e)
         }
     }
 
@@ -43,8 +84,7 @@ export class APIRequester {
             const res = await this.axios.delete(endpoint)
             return res.data
         } catch (e: any) {
-            if ('response' in e && 'data' in e.response) throw new ApiError(e.response.data)
-            throw e
+            throw this.handleError(e)
         }
     }
 
@@ -53,12 +93,21 @@ export class APIRequester {
             const res = await this.axios.get(endpoint)
             return res.data
         } catch (e: any) {
-            if ('response' in e && 'data' in e.response) throw new ApiError(e.response.data)
-            throw e
+            throw this.handleError(e)
         }
+    }
+
+    handleError(e: AxiosError) {
+        if (e instanceof AxiosError) {
+            if (e.status === 401) this.settings.onAuthenticationError()
+            if (e.response && e.response.data) throw new ApiError(e.response.data as any)
+        }
+        throw e
     }
 }
 
 export interface APIRequesterSettings {
-    getToken: () => TokenResponse | undefined
+    getToken: () => TokenResponse | undefined,
+    onAuthenticated: (token: TokenResponse) => void,
+    onAuthenticationError: () => void
 }
--- src/Baaanggg.ts
+++ src/Baaanggg.ts
@@ -11,8 +11,12 @@ import { CoinAPI } from "./api/CoinAPI"
 export class Baaanggg {
     token?: TokenResponse
     private requester = new APIRequester(BASE_URL, {
-        getToken: () => this.getToken()
+        getToken: () => this.getToken(),
+        onAuthenticated: (token) => this.onAuthenticated(token),
+        onAuthenticationError: () => { this.settings?.onAuthenticationError?.() }
     })
+
+    constructor(private settings?: BaaangggSettings) { }
 
     auth = new AuthAPI(this.requester, token => this.onAuthenticated(token))
     admin = new AdminAPI(this.requester)
@@ -22,10 +26,16 @@ export class Baaanggg {
 
     private onAuthenticated(token: TokenResponse) {
         this.token = token
+        if (this.settings?.useLocalStorage) localStorage.setItem('token', JSON.stringify(token))
     }
 
     private getToken() {
-        return this.token
+        let token = this.token
+        if (this.settings?.useLocalStorage) {
+            const tokenJson = localStorage.getItem('token')
+            if (tokenJson != null) token = JSON.parse(tokenJson)
+        }
+        return token
     }
 }
 
@@ -33,4 +43,9 @@ export class ApiError extends Error {
     constructor(response: ErrorResponse) {
         super(response.message)
     }
+}
+
+export class BaaangggSettings {
+    useLocalStorage: boolean = false
+    onAuthenticationError?: () => void
 }
